845cf899e2bab3fee990c6916e8aa988166d5b9e
Abhishek-pratap9120 <abhi911387@gmail.com>
2025-08-07 14:31:06 +0530

    as per ticket request subnet added

--- main.tf
+++ main.tf
@@ -43,5 +43,14 @@ resource "azurerm_subnet" "subnet1" {
  
 }
 
+resource "azurerm_subnet" "subnet2" {
+  name = "subnet12"
+  resource_group_name = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes = "192.168.12.0/24"
+ 
+}
+
+
 
 
