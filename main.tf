d70f83eb636c9450487446c15c8b0624e2b411ce
Mrigendra Kumar <mrigendra.k@outlook.com>
2025-08-15 18:40:15 +0530

    comming in main

--- main.tf
+++ main.tf
@@ -1,72 +1,97 @@
-variable "prefix" {
-  default = "tfvmex"
-}
-
-resource "azurerm_resource_group" "example" {
-  name     = "${var.prefix}-resources"
+resource "azurerm_resource_group" "rgname" {
+  name     = "uat-rgname"
   location = "West Europe"
 }
 
-resource "azurerm_virtual_network" "main" {
-  name                = "${var.prefix}-network"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-network"
+  address_space       = ["192.168.10.0/24"]
+  location            = azurerm_resource_group.rgname.location
+  resource_group_name = azurerm_resource_group.rgname.name
 }
 
-resource "azurerm_subnet" "internal" {
-  name                 = "internal"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.2.0/24"]
+resource "azurerm_subnet" "subnet1" {
+  name                 = "internal1"
+  resource_group_name  = azurerm_resource_group.rgname.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["192.168.10.0/24"]
 }
 
-resource "azurerm_network_interface" "main" {
-  name                = "${var.prefix}-nic"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+resource "azurerm_subnet" "subnet2" {
+  name                 = "internal2"
+  resource_group_name  = azurerm_resource_group.rgname.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["192.168.11.0/24"]
+}
+
+resource "azurerm_network_interface" "internal1" {
+  name                = "internal1-nic"
+  location            = azurerm_resource_group.rgname.location
+  resource_group_name = azurerm_resource_group.rgname.name
 
   ip_configuration {
-    name                          = "testconfiguration1"
-    subnet_id                     = azurerm_subnet.internal.id
+    name                          = "internal1-ipconfig"
+    subnet_id                     = azurerm_subnet.subnet1.id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
-resource "azurerm_virtual_machine" "main" {
-  name                  = "${var.prefix}-vm"
-  location              = azurerm_resource_group.example.location
-  resource_group_name   = azurerm_resource_group.example.name
-  network_interface_ids = [azurerm_network_interface.main.id]
-  vm_size               = "Standard_D2s_v3"
+resource "azurerm_network_interface" "internal2" {
+  name                = "internal2-nic"
+  location            = azurerm_resource_group.rgname.location
+  resource_group_name = azurerm_resource_group.rgname.name
 
-  # Uncomment this line to delete the OS disk automatically when deleting the VM
-  # delete_os_disk_on_termination = true
+  ip_configuration {
+    name                          = "internal2-ipconfig"
+    subnet_id                     = azurerm_subnet.subnet2.id
+    private_ip_address_allocation = "Dynamic"
+  }
 
-  # Uncomment this line to delete the data disks automatically when deleting the VM
-  # delete_data_disks_on_termination = true
+}
+resource "azurerm_linux_virtual_machine" "linuxvm1" {
+  name                = "vmltest01"
+  resource_group_name = azurerm_resource_group.rgname.name
+  location            = azurerm_resource_group.rgname.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  admin_password      = "P@ssw0rd123!"
+  network_interface_ids = [
+    azurerm_network_interface.internal1.id,
+  ]
 
-  storage_image_reference {
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
     publisher = "Canonical"
     offer     = "0001-com-ubuntu-server-jammy"
     sku       = "22_04-lts"
     version   = "latest"
   }
-  storage_os_disk {
-    name              = "myosdisk1"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
+}
+
+resource "azurerm_linux_virtual_machine" "linuxvm2" {
+  name                = "vmltest02"
+  resource_group_name = azurerm_resource_group.rgname.name
+  location            = azurerm_resource_group.rgname.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  admin_password      = "P@ssw0rd123!"
+  network_interface_ids = [
+    azurerm_network_interface.internal2.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
   }
-  os_profile {
-    computer_name  = "hostname"
-    admin_username = "testadmin"
-    admin_password = "Password1234!"
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
   }
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
-  tags = {
-    environment = "staging"
-  }
-} 
+}
